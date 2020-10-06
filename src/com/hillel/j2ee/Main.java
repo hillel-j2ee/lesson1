8abee15c7714656e562c87af615fc71bb5103d71
Ihor Diachenko <divsurf@gmail.com>
2020-10-06 14:57:01 +0300

    Example.

---
+++ src/com/hillel/j2ee/Main.java
@@ -0,0 +1,10 @@
+package com.hillel.j2ee;
+
+import com.hillel.j2ee.logic.Example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Example.printExample();
+    }
+}
---
+++ src/com/hillel/j2ee/logic/Example.java
@@ -0,0 +1,7 @@
+package com.hillel.j2ee.logic;
+
+public class Example {
+    public static void printExample() {
+        System.out.println("This is an example.");
+    }
+}
