136ae8185a2c938dd013dfe5936386c20db82cc2
hillel-j2ee <hillel-j2ee@ukr.net>
2020-10-07 19:17:19 +0300

    Home Work №1 Print log.

--- src/com/hillel/j2ee/Main.java
+++ src/com/hillel/j2ee/Main.java
@@ -1,10 +1,15 @@
 package com.hillel.j2ee;
 
 import com.hillel.j2ee.logic.Example;
+import com.hillel.j2ee.logic.MyLogger;
+import com.hillel.j2ee.logic.VitaliyLogger;
 
 public class Main {
 
     public static void main(String[] args) {
         Example.printExample();
+
+        MyLogger log =  new VitaliyLogger();
+        log.print("Hi");
     }
 }
---
+++ src/com/hillel/j2ee/logic/MyLogger.java
@@ -0,0 +1,7 @@
+package com.hillel.j2ee.logic;
+
+public interface MyLogger {
+
+    void print(String text);
+
+}
