005cd85d27480f6427ab18cc35f39a06c46db0e5
tkenaz <t.kenaz@gmail.com>
2020-10-07 11:44:23 +0300

    Initial commit from MV

---
+++ src/com/hillel/j2ee/logic/MVExample.java
@@ -0,0 +1,8 @@
+package com.hillel.j2ee.logic;
+
+public class MVExample {
+    public static void printMVExample() {
+
+        System.out.println("This is an example from Maryna Vyshnyvetska.");
+    }
+}
