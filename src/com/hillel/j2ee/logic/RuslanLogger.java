dfb5675091800981f6aa04341741fcbbc41bc701
Ruslan Kucherenko <ruslan44kucherenko@gmail.com>
2020-10-11 02:37:54 +0300

    Kucherenko Homework#1

--- src/com/hillel/j2ee/logic/RuslanLogger.java
+++
@@ -1,9 +0,0 @@
-package com.hillel.j2ee.logic;
-
-public class RuslanLogger implements MyLogger {
-
-    @Override
-    public void print(String text) {
-        System.out.println("Ruslan: " + text);
-    }
-}
